0510fb9fc79ddbc6d0e2c832a72e2ad415f78a45
Hidemune Takahashi <h1d3mun3@gmail.com>
2026-08-01 10:17:11 +0900

    Initial Commit

---
+++ lictor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lictor
+//
+//  Created by hidemune on 8/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lictor/lictorApp.swift
@@ -0,0 +1,17 @@
+//
+//  lictorApp.swift
+//  lictor
+//
+//  Created by hidemune on 8/1/26.
+//
+
+import SwiftUI
+
+@main
+struct lictorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
